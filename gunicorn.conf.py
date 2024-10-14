a9b22a02f33ba37f494557603131000746c7ecc1
Jimmy Lange <jamesrobertlange@gmail.com>
2024-10-14 13:09:24 -0500

    further updates for render

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 workers = 2
-worker_class = 'aiohttp.worker.GunicornWebWorker'
+worker_class = 'sync'  # Change this from 'aiohttp.worker.GunicornWebWorker' to 'sync'
 timeout = 300  # 5 minutes
 max_requests = 1000
 max_requests_jitter = 50
